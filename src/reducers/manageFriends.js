03a04839ab6c14937f3d408f1f4b82a7ab3ac175
claire <clairebontempo@gmail.com>
2021-03-06 13:34:00 -0800

    commit tree 723419cc9ff87fe4286b514b781e06f9b92d5bc5

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,9 +1,11 @@
 export function manageFriends(state, action){
     switch (action.type){
         case 'ADD_FRIEND':
-            return {...state, friends: [state.friends, action.friend]}
+            return {...state, 
+                friends: [...state.friends, action.friend]}
         case 'REMOVE_FRIEND':
-            return {friends: state.friends.filter(friend => friend.id !== action.id)}
+            return {...state, 
+                friends: state.friends.filter(friend => friend.id !== action.id)}
         default:
             return state;
     }
